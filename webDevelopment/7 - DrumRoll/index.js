fb9918b9f38f03581ce92a302e235006a7bf7a5e
Ashish Shah1 <Ashish_shah1@epam.com>
2020-01-24 11:59:23 +0530

    Completed JS drum game and practice js code in Practice folder

--- webDevelopment/7 - DrumRoll/index.js
+++ webDevelopment/7 - DrumRoll/index.js
@@ -1,37 +1,45 @@
-
 var totalDrums = document.querySelectorAll(".drum").length;
 
+//detecting buton click
 for(var i = 0;i<totalDrums;i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-        
-        if(i==1){
-            var tom1 = new Audio("sounds/tom-1.mp3");
-            tom1.play();
-        }
-        else if(i==3){
-            var tom2 = new Audio("sounds/tom-2.mp3");
-            tom2.play();
-        }
-        else if(i==2){
-            var tom3 = new Audio("sounds/tom-3.mp3");
-            tom3.play();
-        }
-        else if(i==0){
-            var tom4 = new Audio("sounds/tom-4.mp3");
-            tom4.play();
-        }
-        else if(i==4){
-            var tom5 = new Audio("sounds/snare.mp3");
-            tom5.play();
-        }
-        else if(i==5){
-            var tom6 = new Audio("sounds/crash.mp3");
-            tom6.play();
-        }
-        else{
-            var tom7 = new Audio("sounds/kick-bass.mp3");
-            tom7.play();
-        }
-        
+        makeSound(this.innerHTML);
     });
+}
+
+//detecting keybutton click
+document.addEventListener("keydown",function(event){
+    makeSound(event.key);
+})
+
+// making sound according to the key pressed
+function makeSound(key){
+    if(key == 'w'){
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+    }
+    else if(key == 'a'){
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+    }
+    else if(key == 's'){
+        var tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+    }
+    else if(key == 'd'){
+        var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+    }
+    else if(key == 'j'){
+        var tom5 = new Audio("sounds/snare.mp3");
+        tom5.play();
+    }
+    else if(key == 'k'){
+        var tom6 = new Audio("sounds/crash.mp3");
+        tom6.play();
+    }
+    else{
+        var tom7 = new Audio("sounds/kick-bass.mp3");
+        tom7.play();
+    }
 }
