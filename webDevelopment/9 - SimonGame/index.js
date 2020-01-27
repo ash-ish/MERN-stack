e6d9d6c33ba1923e5c96c649f61adcda419633ce
Ashish Shah1 <Ashish_shah1@epam.com>
2020-01-27 18:10:21 +0530

    added sounds folder; updated JS file;

--- webDevelopment/9 - SimonGame/index.js
+++ webDevelopment/9 - SimonGame/index.js
@@ -1,19 +1,53 @@
+var arrOfColour = ["green","red","blue","yellow"];
+var level = 1;
+var computerInput = [];
+
 $(document).on("keydown",function(){
-    $("h1").text("Level 1");
+    var gameStarted = true;
+    while(gameStarted){
+        $("h1").text("Level " + level);
+        startGame();
+        gameStarted = false;
+    }
 })
 
-var randomNumber = Math.floor(Math.random()*4);
+var count  = 1;
+function startGame(){
+    var gameEnded = false;
+    while(!gameEnded){
+        var randomNumber = Math.floor(Math.random()*4);
+        var colourToBePressed = arrOfColour[randomNumber];
 
-var colourToBePressed;
-if(randomNumber == 0)
-    colourToBePressed = "green";
-else if(randomNumber == 1)
-    colourToBePressed = "red";
-else if(randomNumber == 2)
-    colourToBePressed = "blue";
-else
-    colourToBePressed = "yellow";
+        animateThis(arrOfColour[randomNumber]);
+        playSound(arrOfColour[randomNumber]);
+        
+        computerInput.push(colourToBePressed);
 
-$("." +  colourToBePressed)
-    
-var userInput = [];
+        for(var i = 0;i < count;i++){
+            $(document).on("click",function(){
+                if(! computerInput[i] == this.text()){
+                    restartGame();
+                    gameEnded = true;
+                    break;
+                }
+            });
+        }
+        count++;
+    }
+}
+
+function restartGame(){
+    $("h1").text("Game Over, Press Any Key To Restart");
+    $("body").addClass("bg-red");
+    setTimeout($("body").removeClass("bg-red"),500);
+    playSound("wrong");
+}
+
+function animateThis(colourToBePressed){
+    $("#"+colourToBePressed).fadeIn(100).fadeOut(100).fadeIn(100);
+}
+
+function playSound(colourToBePressed){
+    var tune = new Audio("sounds/" + colourToBePressed + ".mp3");
+    tune.play();
+}
